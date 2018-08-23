8b23d71692991f0aeb248e4e1c8dfe74b4d223d0
jameslee1187@gmail.com <jameslee1187@gmail.com>
2018-08-23 18:28:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,25 @@ def take_a_number (katz_deli, name)
 end
 
 
+$number = 1
+def take_a_number(katz_deli)
+  katz_deli << $number
+  message = "Welcome! You're ticket number #{$number}"
+  $number += 1
+  puts message
+end
+
+# take_a_number(katz_deli) => "Welcome! You're ticket number #1" [1]
+# take_a_number(katz_deli)=> "Welcome! You're ticket number #2" [1,2]
+
+# now_serving(katz_deli)=> "Currently serving: #1" [2]
+# now_serving(katz_deli) => "Currently serving: #2" []
+
+# take_a_number(katz_deli) => "Welcome! You're ticket number #3" [3]
+
+
+
+
 def line (katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
